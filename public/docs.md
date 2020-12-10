5358c3c6c886f2b68ab4a8241c54a071d900836a
ty <ty@notarealemail.com>
2020-12-09 19:03:38 -0800

    Adds testing endpoints. WIP

--- public/docs.md
+++ public/docs.md
@@ -1,7 +1,9 @@
-# Token
+## Users
 
-Getting a token is easy! Make a POST request to `/token` with the bike-id as the
-message body.
+### Token Acquisition
+
+This endpoint allows users to make a POST request to `/token` with the bike-id as the
+message body or a GET request to `/token/<bike-id>`.
 
 Tokens are signed, base64 encoded strings. Decrypted tokens are of the form:
 ```TOK=<bike-id>-<timestamp>```
@@ -15,22 +17,44 @@ the original request.
 
 eg. `TOK=BIKE001-1234567890`
 
-# Timestamp
+## Management
 
-**Not for Production use**
+A bikeshare operator will need to audit the logs on their fleet. These endpoints
+may be helpful in doing so.
+
+### Audit
+
+The endpoint `GET /admin/audit/<token>/<duration>` allows admin users to check whether the time
+of the rental stored on the on the server under the given token (reported by the
+user) match the given time (taken from logs on the bike). The response text indicates audit success or
+failure.
+
+Where:
+
+- `<token>` is the signed, base64 token used to rent the bike.
+- `<duration>` is a string (part of the URI) indicating the integer number of
+  seconds of the rental.
+  
+### Summary
+
+`GET /admin/summary` returns a report of all tokens issued since the server came
+online.
+
+## Development
+
+The following may be useful for development.
+
+### Timestamp
 
 The current system time of the server can be checked with GET `/timestamp`.
 
-# Public Key
- 
-**Not for Production use**
+### Public Key
  
 To get a copy of this server\'s public key. Make a GET request to `/cert`.
  
-# Testing
+### Testing
 
-**Not for Production use**
- 
 To get back an invalid token (of an expected format for testing purposes), Make
 a GET request to `/test`. The token will be formed with bike id: INVALID-TEST-BIKEID and timestamp 0.
  
+```TOK=INVALID-TEST-BIKEID-0```
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,111 @@
+use std::collections::HashMap;
 use std::io;
 use std::fs;
+use std::sync::Mutex;
 use std::time;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_cors::Cors;
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 mod token;
+
+// User Endpoints
+
+fn token(log: web::Data<Mutex<HashMap<String, Option<u32>>>>, bike_id: String) -> HttpResponse {
+    match time::SystemTime::now().duration_since(time::SystemTime::UNIX_EPOCH) {
+        Ok(sys_time) => {
+            let plain_token = token::build(bike_id, sys_time);
+            let crypt_token = token::encrypt(&plain_token);
+
+            {
+                let token = crypt_token.clone();
+                let mut log = log.lock().expect("Unable to lock log");
+
+                log.insert(token, None);
+            }
+
+            HttpResponse::Ok().body(crypt_token)
+        },
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("{:?}", e))
+        },
+    }
+}
+
+fn finalize(log: web::Data<Mutex<HashMap<String, Option<u32>>>>, token: web::Path<String>, duration: web::Path<u32>) -> HttpResponse {
+    let token = token.into_inner();
+    let duration = duration.into_inner();
+
+    let mut log = log.lock().expect("Unable to lock log");
+
+    match log.get(&token) {
+        // This nutty-ness b.c. get returns Option<Val>
+        Some(None) => {
+            log.remove(&token);
+            log.insert(token, Some(duration));
+
+            HttpResponse::Ok().body("Ride finalized.")
+        },
+        Some(Some(_dur)) =>
+            HttpResponse::Forbidden().body("Ride already finalized. Not updated."),
+        None =>
+            HttpResponse::BadRequest().body("No record for given token."),
+    }
+}
+
+// Mgmt Endpoints
+
+fn audit(log: web::Data<Mutex<HashMap<String, Option<u32>>>>, token: web::Path<String>, check_dur: web::Path<u32>) -> HttpResponse {
+    let token = token.into_inner();
+    let check_dur = check_dur.into_inner();
+
+    let log = log.lock().expect("Unable to lock log");
+
+    match log.get(&token) {
+        // This nutty-ness b.c. get returns Option<Val>
+        Some(Some(server_dur)) if *server_dur == check_dur =>
+            HttpResponse::Ok().body("Audit OK. Values match"),
+        Some(Some(_dur)) =>
+            HttpResponse::Ok().body("Fraud detected. Given value does not match value on record."),
+        Some(None) =>
+            HttpResponse::Ok().body("Possible Fraud. No update for given token."),
+        None =>
+            HttpResponse::BadRequest().body("No record for given token."),
+    }
+}
+
+fn summary(log: web::Data<Mutex<HashMap<String, Option<u32>>>>) -> HttpResponse {
+    let mut rept = String::new();
+    rept.push_str("<html>");
+    rept.push_str("<body>");
+    rept.push_str("<table>");
+    rept.push_str("<tr><th>Row</th><th>Token</th><th>Time</th></tr>");
+
+    {
+        let log = log.lock().expect("Unable to acquire mutex lock");
+
+        for (n, (tok, time)) in log.iter().enumerate() {
+            let mut short_tok = tok.clone();
+            short_tok.truncate(20);
+
+            let row = match time {
+                Some(time) =>
+                    format!("<tr><th>{}</th><th>{}...</th><th>{}</th></tr>", n+1, short_tok, time),
+                None =>
+                    format!("<tr><th>{}</th><th>{}...</th><th>*Not Finalized*</th></tr>", n+1, short_tok),
+            };
+            rept.push_str(row.as_str());
+        }
+    }
+
+    rept.push_str("</table>");
+    rept.push_str("</body>");
+    rept.push_str("</html>");
+
+    HttpResponse::Ok().body(rept)
+}
+
+// Dev  Endpoints
 
 fn test() -> HttpResponse {
     let p_token = token::build(String::from("INVALID-TEST-BIKEID"),
@@ -16,19 +116,14 @@ fn test() -> HttpResponse {
     HttpResponse::Ok().body(c_token)
 }
 
-fn token(bike_id: String) -> HttpResponse {
-    match time::SystemTime::now().duration_since(time::SystemTime::UNIX_EPOCH) {
-        Ok(sys_time) => {
-            let plain_token = token::build(bike_id, sys_time);
-            let crypt_token = token::encrypt(&plain_token);
+fn timestamp() -> HttpResponse {
+    let t = time::SystemTime::now().duration_since(time::SystemTime::UNIX_EPOCH).unwrap();
 
-            HttpResponse::Ok().body(crypt_token)
-        },
-        Err(e) => {
-            HttpResponse::InternalServerError().body(format!("{:?}", e))
-        },
-    }
+    HttpResponse::Ok().body(format!("We are currently {} seconds from 1970-01-01 00:00",
+                                    t.as_secs()))
 }
+
+// Helper(s)
 
 fn file(f_name: &str) -> HttpResponse {
     match fs::read_to_string(f_name) {
@@ -37,13 +132,6 @@ fn file(f_name: &str) -> HttpResponse {
         Err(e) =>
             HttpResponse::InternalServerError().body(format!("{:?}", e)),
     }
-}
-
-fn timestamp() -> HttpResponse {
-    let t = time::SystemTime::now().duration_since(time::SystemTime::UNIX_EPOCH).unwrap();
-
-    HttpResponse::Ok().body(format!("We are currently {} seconds from 1970-01-01 00:00",
-                                    t.as_secs()))
 }
 
 #[actix_web::main]
@@ -56,18 +144,32 @@ async fn main() -> io::Result<()> {
     ssl_builder.set_certificate_chain_file("/etc/letsencrypt/live/getyrtokens.ddns.net/fullchain.pem")
                .expect("Couldn't find the chain file for SSL cxn");
 
-    HttpServer::new(|| {
+    let token_log =
+        web::Data::new(
+            Mutex::new(
+                HashMap::<String, Option<u32>>::new()));
+
+    HttpServer::new(move || {
         App::new()
-            .route("/",      web::get().to(|| {file("public/docs.html")}))
-            .route("/test",  web::get().to(test))
-            .route("/timestamp",  web::get().to(timestamp))
-            .route("/cert",  web::get().to(|| {file("id_rsa_pem.pub")}))
-            .route("/token", web::post().to(token))
+            .app_data(token_log.clone())
+        // User Endpoints
+            .route("/token",          web::post().to(token))
+            .route("/token/{bikeid}", web::get().to(token))
+            .route("/finalize/{token}/{duration}", web::get().to(finalize))
+        // Management Endpoints
+            .route("/admin/audit/{token}/{duration}", web::get().to(audit))
+            .route("/admin/summary",                  web::get().to(summary))
+        // Dev Endpoints
+            .route("/",          web::get().to(|| {file("public/docs.html")}))
+            .route("/cert",      web::get().to(|| {file("id_rsa_pem.pub")}))
+            .route("/style.css", web::get().to(|| {file("public/style.css")}))
+            .route("/test",      web::get().to(test))
+            .route("/timestamp", web::get().to(timestamp))
             .wrap(
                 Cors::default()
             )
     })
-    .bind("0.0.0.0:80")?
+    .bind("0.0.0.0:8000")?
     .bind_openssl("0.0.0.0:443", ssl_builder)?
     .run()
     .await
